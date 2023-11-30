10009de4fc4c8cec3dd3c76ffa08672cbbdac313
Ayories <sanusiabdulquadri8@gmail.com>
2023-11-30 17:32:21 +0100

    first web server

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const greet = (name) => {    
+    console.log(`hello ${name} how are you doing`)
+}
+// greet(David)
+const calculator = (name,a,b) => {
+    greet(David)
+    console.log(`hello ${name} how are you doing`)
+}

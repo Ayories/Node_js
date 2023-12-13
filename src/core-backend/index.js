596f4e6f92b0e6457aa6c6f7c091d91f3a8dbfac
Ayories <sanusiabdulquadri8@gmail.com>
2023-12-13 18:11:34 +0100

    new update

---
+++ src/core-backend/index.js
@@ -0,0 +1,22 @@
+const greet = (name) => {    
+    console.log(`hello ${name} how are you doing`)
+}
+// greet(David)
+const calculator = (name,a,b) => {
+    greet(David)
+    console.log(`hello ${name} how are you doing`)
+}
+
+// import http from "http"
+
+// const server = http.createServer((req, res) => {
+//     res.end("hello this is my first server!!!!")
+// }
+// )
+
+// const port = 3001
+// const host = "127.0.0.1"
+
+// server.listen(3001, "127.0.0.1", ()=>{
+//     console.log(`Server connection to port:${port} and host:${host} established`);
+// })

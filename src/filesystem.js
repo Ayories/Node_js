d1fb7c6cf7cfd0a05e107f7a666686bb4a0143b4
Ayories <sanusiabdulquadri8@gmail.com>
2023-12-13 16:10:09 +0100

    weekly updates

--- src/filesystem.js
+++ src/filesystem.js
@@ -5,11 +5,11 @@ fs.readFile("test.txt", "utf-8", (err, data) => {
     console.log(data)
 })
 
-// fs.writeFile("testWrite.txt", "Hello World, welcome you have been rick rolled!!! dumdumdum", (err) => {
-//     if (err) console.error(err)
+fs.writeFile("testWrite.txt", "Hello World, welcome you have been rick rolled!!! dumdumdum", (err) => {
+    if (err) console.error(err)
     
-//     else{
-//         console.log("file written");
-//     }
-// })
+    else{
+        console.log("file written");
+    }
+})
 
